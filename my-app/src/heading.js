3f062ff0bffb96cf8b74da062b1570a3051ec703
Sunilchow19 <you@example.com>
2024-10-15 23:01:27 +0530

    Second Commit

--- my-app/src/heading.js
+++ my-app/src/heading.js
@@ -1,8 +1,16 @@
 let Head=()=>{
+
+    const myStyle = {
+        color: "white",
+        backgroundColor: "DodgerBlue",
+        padding: "10px",
+        fontFamily: "Sans-Serif"
+      };
+
     return (
         <>
 
-        <h1><center><i><u>RESUME</u></i></center></h1>
+        <h1 style={myStyle}><center><i><u>RESUME</u></i></center></h1>
 
         </>
     )
--- my-app/src/name.js
+++ my-app/src/name.js
@@ -1,7 +1,14 @@
 function Name(){
+
+    const myStyle = {
+        color: "white",
+        backgroundColor: "#E380FF",
+        padding: "10px",
+        fontStyle: "Italic"
+      };
     return (
         <div>
-            <h2><center>Venkata Sunil.S</center></h2>
+            <h2 style={myStyle}><center>Venkata Sunil.S</center></h2>
         </div>
     )
 }
